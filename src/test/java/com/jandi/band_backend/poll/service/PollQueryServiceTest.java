40b6a72bb063714b97cc920e2425ee1cbce7667a
Kim Keon-Young <diamondgonny@gmail.com>
2025-08-04 17:23:33 +0900

    feat: Poll API에 동아리 멤버 권한 검증 추가
    - 투표 목록, 상세, 결과 조회 시 동아리 멤버 검증
    - 비인증 사용자의 접근 차단
    - Team API와 일관성 있는 보안 정책 적용
    - Club이 null인 경우 방어 코드 추가

--- src/test/java/com/jandi/band_backend/poll/service/PollQueryServiceTest.java
+++ src/test/java/com/jandi/band_backend/poll/service/PollQueryServiceTest.java
@@ -4,6 +4,8 @@ import com.jandi.band_backend.club.entity.Club;
 import com.jandi.band_backend.global.exception.ClubNotFoundException;
 import com.jandi.band_backend.global.exception.PollNotFoundException;
 import com.jandi.band_backend.global.util.EntityValidationUtil;
+import com.jandi.band_backend.global.util.UserValidationUtil;
+import com.jandi.band_backend.global.util.PermissionValidationUtil;
 import com.jandi.band_backend.poll.dto.PollDetailRespDTO;
 import com.jandi.band_backend.poll.dto.PollRespDTO;
 import com.jandi.band_backend.poll.entity.Poll;
@@ -51,6 +53,12 @@ class PollQueryServiceTest {
 
     @Mock
     private EntityValidationUtil entityValidationUtil;
+
+    @Mock
+    private UserValidationUtil userValidationUtil;
+
+    @Mock
+    private PermissionValidationUtil permissionValidationUtil;
 
     private Club testClub;
     private Users testUser;
@@ -106,7 +114,7 @@ class PollQueryServiceTest {
                 .thenReturn(pollPage);
 
         // When
-        Page<PollRespDTO> result = pollService.getPollsByClub(1, pageable);
+        Page<PollRespDTO> result = pollService.getPollsByClub(1, 1, pageable);
 
         // Then
         assertNotNull(result);
@@ -134,7 +142,7 @@ class PollQueryServiceTest {
 
         // When & Then
         assertThrows(ClubNotFoundException.class,
-                () -> pollService.getPollsByClub(999, pageable));
+                () -> pollService.getPollsByClub(999, 1, pageable));
 
         verify(entityValidationUtil).validateClubExists(999);
         verify(pollRepository, never()).findAllByClubAndDeletedAtIsNullOrderByCreatedAtDesc(any(), any());
@@ -151,7 +159,7 @@ class PollQueryServiceTest {
                 .thenReturn(emptyPage);
 
         // When
-        Page<PollRespDTO> result = pollService.getPollsByClub(1, pageable);
+        Page<PollRespDTO> result = pollService.getPollsByClub(1, 1, pageable);
 
         // Then
         assertNotNull(result);
@@ -324,7 +332,7 @@ class PollQueryServiceTest {
 
         // When & Then
         assertThrows(RuntimeException.class,
-                () -> pollService.getPollsByClub(1, pageable));
+                () -> pollService.getPollsByClub(1, 1, pageable));
 
         verify(entityValidationUtil).validateClubExists(1);
         verify(pollRepository).findAllByClubAndDeletedAtIsNullOrderByCreatedAtDesc(testClub, pageable);
