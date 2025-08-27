e6ba63ab3860aecaf4079381abeaf5d6d60a9362
hhhan-jh <jeo4924@gmail.com>
2025-08-27 22:03:59 +0900

    Revert "chore: 에러 메시지 수정"
    This reverts commit c1e8987606e179ba0fb2eaa29a5d27f97e5ece5b.

--- src/main/java/com/jandi/band_backend/poll/controller/PollController.java
+++ src/main/java/com/jandi/band_backend/poll/controller/PollController.java
@@ -97,7 +97,7 @@ public class PollController {
             @PathVariable String emoji,
             @RequestParam(required = false) String code,
             @AuthenticationPrincipal CustomUserDetails userDetails) {
-        if (code != null) {
+        if (code != null) { // 외부인: 코드 검증 필요
             joinService.verifyPollCode(code, pollId);
             inviteCodeService.deleteRecord(code);
         }
@@ -105,7 +105,7 @@ public class PollController {
             Integer userId = userDetails.getUserId();
             Integer pollsClubId = inviteUtilService.getPollsClubId(pollId);
             if(!inviteUtilService.isMemberOfClub(pollsClubId, userId))
-                throw new InvalidAccessException("투표 권한이 없습니다: 동아리원이 아닙니다");
+                throw new InvalidAccessException("팀원만 투표할 수 있습니다");
         }
         PollSongRespDTO responseDto = pollService.setVoteForSong(pollId, songId, emoji, userDetails.getUserId());
         return ResponseEntity.ok(CommonRespDTO.success("투표가 설정되었습니다.", responseDto));
